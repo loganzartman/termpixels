0570863588818444d9aa9c32affafaeb3815a1e8
Logan Zartman <logan.zartman@utexas.edu>
2019-02-10 00:50:19 -0600

    WIP

--- termpixels/terminfo.py
+++ termpixels/terminfo.py
@@ -1,8 +1,12 @@
 import os
 from os.path import expanduser, exists
+import struct
+import re
 from terminfo_indices import boolean_indices, number_indices, string_indices
 
 TERMINFO_PATHS = ["/usr/share/terminfo", "/usr/lib/terminfo", expanduser("~/.terminfo"), "/etc/terminfo"]
+if "TERMINFO" in os.environ: 
+    TERMINFO_PATHS.append(os.environ["TERMINFO"], 0)
 
 class Terminfo:
     def __init__(self, *, names, booleans, numbers, strings):
@@ -43,46 +47,50 @@ def parse_terminfo(path):
     See `man 5 term`
     """
     with open(path, "rb") as f:
-        magic = _read_int(f)
+        magic = _read_short(f)
         if magic == 542:
-            return _parse_terminfo_base(f, extended=True)
+            return _parse_terminfo_base(f, extended_numbers=True)
         if magic == 282:
-            return _parse_terminfo_base(f, extended=False)
+            return _parse_terminfo_base(f, extended_numbers=False)
         raise Exception("Invalid magic number")
 
-def _read_int(f, size=2):
+def _read_short(f):
+    """Read little-endian short.
+    """
+    return struct.unpack("<h", f.read(2))[0]
+
+def _read_int(f):
     """Read little-endian integer.
     """
-    bites = [b for b in f.read(size)]
-    num = 0
-    if all(b == 255 for b in bites):
-        return -1
-    for i, b in enumerate(bites):
-        num |= b << (8 * i)
-    return num
+    return struct.unpack("<i", f.read(4))[0]
 
 def _eat_null(f):
     old = f.tell()
     if f.read(1) != "\x00":
         f.seek(old)
 
-def _parse_terminfo_base(f, extended=False):
-    names_bytes = _read_int(f)    # the size, in bytes, of the names section
-    booleans_count = _read_int(f) # the number of bytes in the boolean section
-    numbers_count = _read_int(f)  # the number of short integers in the numbers section
-    offsets_count = _read_int(f)  # the number of offsets (short integers) in the strings section
-    table_bytes = _read_int(f)    # the size, in bytes, of the string table
+def _parse_terminfo_base(f, extended_numbers=False):
+    names_bytes = _read_short(f)    # the size, in bytes, of the names section
+    booleans_count = _read_short(f) # the number of bytes in the boolean section
+    numbers_count = _read_short(f)  # the number of short integers in the numbers section
+    offsets_count = _read_short(f)  # the number of offsets (short integers) in the strings section
+    table_bytes = _read_short(f)    # the size, in bytes, of the string table
 
     names = f.read(names_bytes).decode("ascii").rstrip("\0").split("|")
-    booleans = list(map(bool, f.read(booleans_count)))
+    booleans = [bool(b) for b in f.read(booleans_count)]
 
     _eat_null(f) # eat alignment byte if present
 
-    numbers = [_read_int(f, 4 if extended else 2) for _ in range(numbers_count)]
+    numbers = [_read_int(f) if extended_numbers else _read_short(f) for _ in range(numbers_count)]
 
-    offsets = [_read_int(f) for _ in range(offsets_count)]
-    string_table = f.read(table_bytes).decode("ascii")
+    offsets = [_read_short(f) for _ in range(offsets_count)]
+    print(offsets)
+    string_table = f.read(table_bytes).decode("ascii") + "\0" # extra null terminator to simplify next line
     strings = []
-    for i, o in enumerate(offsets):
-        print(i, o, string_table[o:string_table.index("\0", o)])
+    for o in offsets:
+        if o < 0:
+            strings.append(None)
+        strings.append(string_table[o:string_table.index("\0", o)])
     return Terminfo(names=names, booleans=booleans, numbers=numbers, strings=strings)
+
+get_terminfo()
