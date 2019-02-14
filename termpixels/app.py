8934af3a2f07d742c05a716103d02a7f7cbedead
Logan Zartman <logan.zartman@utexas.edu>
2019-02-13 18:14:25 -0600

    refactor app.py for general purpose use

--- termpixels/app.py
+++ termpixels/app.py
@@ -5,25 +5,29 @@ from detector import detect_backend, detect_input
 class App:
     def __init__(self):
         self.backend = detect_backend()
+        self.backend.listen("resize", lambda _: self.on_resize())
         self.input = detect_input()
-        self.input.listen("key", self.on_key)
+        self.input.listen("key", lambda d: self.on_key(d))
         self.screen = Screen(self.backend)
-        self.cursor_x = 0
-
-    def on_key(self, ch):
-        self.screen.print(ch, self.cursor_x, 1)
-        self.screen.cursor_pos = (self.cursor_x + 1, 1)
-        self.screen.update()
-        self.cursor_x += 1
     
     def start(self):
         self.input.start()
-        self.screen.print("Type something:", 0, 0)
-        self.screen.update()
         try:
             while True:
+                self.on_frame()
                 sleep(1/60)
         except KeyboardInterrupt:
             pass
         finally:
-            self.input.stop() 
+            self.screen.show_cursor = True
+            self.screen.update()
+            self.input.stop()
+    
+    def on_key(self, data):
+        pass
+    
+    def on_resize(self):
+        pass
+    
+    def on_frame(self):
+        pass
--- termpixels/test-app.py
+++ termpixels/test-app.py
@@ -1,4 +1,11 @@
 from app import App
 
-app = App()
-app.start()
+class TestApp(App):
+    def __init__(self):
+        super().__init__()
+    
+    def on_resize(self):
+        self.screen.print("{} x {}    ".format(self.screen.w, self.screen.h), 1, 1)
+        self.screen.update()
+
+TestApp().start()
