db4f882abd2a41bbf25e8f65d15d05cf35294002
Logan Zartman <logan.zartman@utexas.edu>
2019-02-25 15:26:45 -0600

    update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="termpixels-logan-z",
+    name="termpixels",
     version="0.0.1",
     author="Logan",
     author_email="logan.zartman@utexas.edu",
