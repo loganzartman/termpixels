0596eeaa5064a9600066d2d450c365632f781848
Logan Zartman <logan.zartman@utexas.edu>
2019-03-20 18:20:15 -0500

    switched back to cache-based (not dirty-based) screen update optimization

--- termpixels/screen.py
+++ termpixels/screen.py
@@ -132,6 +132,7 @@ class Screen:
         with self.lock:
             self._pixels = [[self._pixels[x][y] if x < self._w and y < self._h else PixelData() 
                             for y in range(h)] for x in range(w)]
+            self._pixels_cache = [[PixelData(fg=None, bg=None, char=" ") for y in range(h)] for x in range(w)]
             self._w = w
             self._h = h
             self.backend.clear_screen()
@@ -171,7 +172,11 @@ class Screen:
                         pixel.char = char
     
     def clear(self, *, fg=Color(255,255,255), bg=Color(0,0,0), char=" "):
-        self.fill(0, 0, self.w, self.h, fg=fg, bg=bg, char=char)
+        with self.lock:
+            blank = PixelData(fg=fg, bg=bg, char=char)
+            for i in range(0, self.w):
+                for j in range(0, self.h):
+                    self._pixels[i][j].set(blank)
     
     def update(self):
         with self.lock:
@@ -180,9 +185,9 @@ class Screen:
             for y in range(self.h):
                 for x in range(self.w):
                     pixel = self._pixels[x][y]
-                    if pixel._dirty:
+                    if pixel != self._pixels_cache[x][y]:
                         self.render(pixel, x, y)
-                        pixel._dirty = False
+                        self._pixels_cache[x][y].set(pixel)
                         self._update_count += 1
             self.backend.cursor_pos = self.cursor_pos
             self.backend.flush()
@@ -215,7 +220,7 @@ class Screen:
 
 class PixelData:
     def __init__(self, *, fg=Color(255, 255, 255), bg=Color(0, 0, 0), char=" "):
-        self._dirty = True
+        self._hash = None
         self._char = None
         self._fg = None
         self._bg = None
@@ -233,7 +238,7 @@ class PixelData:
             raise Exception("Character must have length 1")
         if self._char != char:
             self._char = char
-            self._dirty = True
+            self._hash = None
     
     @property
     def fg(self):
@@ -243,7 +248,7 @@ class PixelData:
     def fg(self, value):
         if self._fg != value:
             self._fg = value
-            self._dirty = True
+            self._hash = None
     
     @property
     def bg(self):
@@ -253,13 +258,23 @@ class PixelData:
     def bg(self, value):
         if self._bg != value:
             self._bg = value
-            self._dirty = True
+            self._hash = None
+    
+    def set(self, pixel):
+        self._fg = pixel._fg
+        self._bg = pixel._bg
+        self._char = pixel._char
+        self._hash = None
 
     def __str__(self):
         return "({}; fg {}; bg {})".format(self.char, self.fg, self.bg)
     
     def __eq__(self, other):
-        try:
-            return self.fg == other.fg and self.bg == other.bg and self.char == other.char
-        except AttributeError:
+        if hash(self) != hash(other):
             return False
+        return self.fg == other.fg and self.bg == other.bg and self.char == other.char
+    
+    def __hash__(self):
+        if self._hash is None:
+            self._hash = hash((self.char, self.fg, self.bg))
+        return self._hash
