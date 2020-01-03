f550c9fa993f6f175d077e8c5a4ffade52203d36
Logan Zartman <logan.zartman@utexas.edu>
2020-01-02 22:38:30 -0600

    update drawbox.py example

--- termpixels/examples/drawbox.py
+++ termpixels/examples/drawbox.py
@@ -1,5 +1,6 @@
 from termpixels import App, Buffer, Color
 from termpixels.drawing import draw_box, BOX_CHARS_DOUBLE
+from termpixels.drawing import draw_frame, FRAME_CHARS_HEAVY
 from termpixels.util import corners_to_box
 from time import perf_counter
 
@@ -16,12 +17,17 @@ def main():
         if drag_start is None or mouse_pos is None:
             return
         x, y, w, h = corners_to_box(*drag_start, *mouse_pos)
-        draw_box(buffer, x, y, w, h, chars=BOX_CHARS_DOUBLE, fg=Color.hsl(perf_counter(), 1.0, 0.5))
+        # draw_box(buffer, x, y, w, h, chars=BOX_CHARS_DOUBLE, fg=Color.hsl(perf_counter(), 1.0, 0.5))
+        draw_frame(buffer, x, y, w, h, fg=Color.hsl(perf_counter(), 1.0, 0.5))
 
     @app.on("start")
     @app.on("resize")
     def on_resize():
         buf.resize(app.screen.w, app.screen.h)
+        for x in range(buf.w):
+            for y in range(buf.h):
+                col = Color.rgb(0,0,0) if (x + y) % 2 else Color.rgb(0.1,0.1,0.1)
+                buf.put_char(" ", x, y, bg=col)
 
     @app.on("mouse")
     def on_mouse(mouse):
