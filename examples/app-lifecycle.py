3fe3366f4b57fe2374bcfbb4618b25b9a3787e65
Logan Zartman <logan.zartman@utexas.edu>
2020-01-04 17:26:08 -0600

    cleanup lifecycle example

--- examples/app-lifecycle.py
+++ examples/app-lifecycle.py
@@ -2,38 +2,60 @@ from termpixels import App
 from time import sleep
 
 def main():
-    app = App()
+    app = App(mouse=True)
+    frame_num = 0
+    size_text = ""
 
     @app.on("start")
-    def on_start():
-        app.screen.print("App start", 0, 0)
+    def start():
+        app.screen.show_cursor = True
+        app.screen.print("App start, waiting 1 second.", 0, 0)
         app.screen.update()
         sleep(1)
     
-    frame_num = 0
-
     @app.on("frame")
-    def on_frame():
+    def frame():
         nonlocal frame_num
-        app.screen.print("Frame {}".format(frame_num), 0, 1)
+        app.screen.clear()
+        app.screen.print(f"Frame: {frame_num}", 0, 0)
+        app.screen.print("Press 'q' to quit.", 0, 1)
+        app.screen.print(size_text, 0, 2)
         app.screen.update()
         frame_num += 1
-        if frame_num > 10:
+
+    @app.on("resize")
+    def resize():
+        nonlocal size_text
+        size_text = f"Resized to {app.screen.w}x{app.screen.h}"
+
+    @app.on("key")
+    def key(k):
+        if k == "q":
             app.stop()
     
+    @app.on("mouse")
+    def mouse(m):
+        app.screen.cursor_pos = (m.x, m.y)
+    
     @app.on("before_stop")
-    def on_before_stop():
-        app.screen.print("Before stop", 0, 2)
+    def before_stop():
+        # Terminal is not reset; we can use the screen.
+        app.screen.clear()
+        app.screen.print("Before stop", 0, 0)
         app.screen.update()
         sleep(0.5)
     
     @app.on("after_stop")
-    def on_after_stop():
-        app.screen.print("After stop", 0, 3)
-        app.screen.update()
+    def after_stop():
+        # Terminal is reset; we can print normally.
+        print("After stop")
     
-    app.start()
-    app.await_stop()
+    # the following two method calls are equivalent to calling app.run()
+    app.start() # this does not block
+    # you could run code concurrently with the App here, but you should not
+    # interact with its attributes outside of event listeners due to potential
+    # concurrency issues.
+    app.await_stop() # this blocks
     print("Exited")
 
 if __name__ == "__main__":
